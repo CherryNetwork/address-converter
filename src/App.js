c93655b121b549a7d0994455e90968612314910c
Vinayak Dhadda <vinayak@Vinayaks-MacBook-Pro.local>
2023-04-20 16:50:45 +0100

    delete SNOW and Arctic from the dropdown and from the conversion and changes text to Cherry Address Converter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import { useState } from 'react';
 
-import {convertH160ToSs58, convertSs58ToH160, 
-  ADDRESS_FORMAT, encodePubKey, CHAIN_PREFIX, encodePolkadotAddress, validateSs58, getPubKey} from './utils';
+import {
+  convertH160ToSs58,
+  convertSs58ToH160,
+  ADDRESS_FORMAT,
+  encodePubKey,
+  CHAIN_PREFIX,
+  encodePolkadotAddress,
+  validateSs58,
+  getPubKey,
+} from './utils';
 
 import './App.css';
 
-
-
 function App() {
-
-  const [inputAddrFormat, setInputAddrFormat] = useState(ADDRESS_FORMAT.snow);
+  const [inputAddrFormat, setInputAddrFormat] = useState(ADDRESS_FORMAT.ss58);
   const [inputAddress, setInputAddress] = useState('');
   const [outputAddress, setOutputAddress] = useState('');
   const [error, setError] = useState('');
 
-  function handleInputFormatChange (e) {
+  function handleInputFormatChange(e) {
     setInputAddrFormat(e.target.value);
   }
 
-  function handleInputAddrChange (e) {
+  function handleInputAddrChange(e) {
     setInputAddress(e.target.value);
   }
 
   function getAllAddresses(ss58Address, defaultH160) {
     const res = {};
-    res[ADDRESS_FORMAT.snow] = encodePolkadotAddress(ss58Address, CHAIN_PREFIX.snow);
-    res[ADDRESS_FORMAT.arctic] = encodePolkadotAddress(ss58Address, CHAIN_PREFIX.arctic);
-    res[ADDRESS_FORMAT.h160] = defaultH160 ? defaultH160 : convertSs58ToH160(ss58Address);
+
+    res[ADDRESS_FORMAT.h160] = defaultH160
+      ? defaultH160
+      : convertSs58ToH160(ss58Address);
     res[ADDRESS_FORMAT.ss58] = ss58Address;
     res[ADDRESS_FORMAT.pubKey] = getPubKey(ss58Address);
 
@@ -37,12 +43,12 @@ function App() {
     const outputArray = [];
     Object.entries(addressObj).forEach((entry) => {
       outputArray.push(`<b>${entry[0]}</b>: \t${entry[1]}<br/><br/>`);
-    })
+    });
 
     return outputArray.join('\n');
   }
 
-  function handleSubmit (e) {
+  function handleSubmit(e) {
     e.preventDefault();
     try {
       const inputAddressTrimmed = inputAddress.trim();
@@ -53,24 +59,17 @@ function App() {
         // convert h160 to ss58
         defaultH160 = inputAddressTrimmed; // limitation of bidirectional H160 mapping
         eqvSs58Addr = convertH160ToSs58(inputAddressTrimmed);
-
       } else if (inputAddrFormat === ADDRESS_FORMAT.pubKey) {
         // convert pubkey to ss58
         eqvSs58Addr = encodePubKey(inputAddressTrimmed, CHAIN_PREFIX.ss58);
-
-      } else if(inputAddrFormat === ADDRESS_FORMAT.snow) {
-        // convert SNOW to ss58
-        eqvSs58Addr = encodePolkadotAddress(inputAddressTrimmed, CHAIN_PREFIX.ss58);
-      } else if(inputAddrFormat === ADDRESS_FORMAT.arctic) {
-        // convert arctic to ss58
-        eqvSs58Addr = encodePolkadotAddress(inputAddressTrimmed, CHAIN_PREFIX.ss58);
       }
 
       validateSs58(eqvSs58Addr);
 
-      setOutputAddress(formatAddresses(getAllAddresses(eqvSs58Addr, defaultH160)));
+      setOutputAddress(
+        formatAddresses(getAllAddresses(eqvSs58Addr, defaultH160))
+      );
       setError('');
-
     } catch (e) {
       console.error(e);
       if (typeof e === 'string') {
@@ -83,39 +82,36 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <form onSubmit={handleSubmit}>
-        <div className="form-group">
+        <div className='form-group'>
           <label className='form-label'>Input address format: </label>
           <select value={inputAddrFormat} onChange={handleInputFormatChange}>
-            <option value={ADDRESS_FORMAT.snow}>SNOW Mainnet</option>
-            <option value={ADDRESS_FORMAT.arctic}>Arctic Testnet</option>
             <option value={ADDRESS_FORMAT.ss58}>SS58 (Substrate)</option>
             <option value={ADDRESS_FORMAT.h160}>H160 (Etheruem)</option>
             <option value={ADDRESS_FORMAT.pubKey}>Public Key (Global)</option>
           </select>
         </div>
 
-        <div className="form-group">
+        <div className='form-group'>
           <label className='form-label'>Address: </label>
-          <input 
+          <input
             type='text'
             value={inputAddress}
             onChange={handleInputAddrChange}
           />
         </div>
 
-        <div className="form-group">
+        <div className='form-group'>
           <button type='submit'>Go!</button>
         </div>
-        
-        <div className='error'>
-          {error}
-        </div>
 
+        <div className='error'>{error}</div>
       </form>
-      <div className='output-address' dangerouslySetInnerHTML={{__html: outputAddress}} />
-
+      <div
+        className='output-address'
+        dangerouslySetInnerHTML={{ __html: outputAddress }}
+      />
     </div>
   );
 }
